5b49838f5c7542700d8654a03f2b0aa3e3af5cc6
Gocho Mugo <mugo@forfuture.co.ke>
2015-02-05 20:40:32 +0300

    Better command-line runner
    The command-line runner has been revamped to be extensible and remove
    any logic from the "view".

--- remindme/remindme.py
+++ remindme/remindme.py
@@ -1,29 +1,21 @@
 '''
-The Core of RemindMe
-~~~~~~~~~~~~~~~~~~~~
-Implemented as pythonic as possible
-
-Copyright (c) 2014-2015 Gocho Mugo <mugo@forfuture.co.ke>
+Command-line runner for Application
 '''
 
 import argparse
-import colorama
-import getpass
 import sys
-from . import db
+from . import config
+from . import models
+from . import utils
 
-__version__ = '0.2.1'
-LICENSE = "https://github.com/GochoMugo/remindme/blob/master/LICENSE"
-_default = colorama.Fore.WHITE
-_error = colorama.Fore.RED
-_priority = colorama.Fore.MAGENTA
-_reset = colorama.Style.RESET_ALL
-_success = colorama.Fore.GREEN
-PY2 = sys.version_info[0] == 2
-PY3 = sys.version_info[0] == 3
+
+# start-up activities
+console = models.Console("runner")
+database = models.RemindmeDatabase(config.PATHS["db_file"])
 
 
 def arg_parser():
+    '''Argument Parser.'''
     parser = argparse.ArgumentParser(
         description='Reminds you of something you knew before',
         epilog="See LICENSE at {0}".format(LICENSE)
@@ -58,116 +50,88 @@ def arg_parser():
     return args
 
 
-def print_out(_status, content, newline=True):
-    words = '{0}{1}{2}'
-    words += "\n" if newline is True else ""
-    sys.stdout.write(words.format(_status, content, _reset))
-    sys.stdout.flush()
-
-
-def get_input(question=""):
-    if PY2:
-        return raw_input(question + '> ')
-    else:
-        return input(question + '> ')
-
-
 def run():
+    '''Run the command-line runner.'''
     args = arg_parser()
-    content = db.read()
 
     if args['list']:
-        keywords = [item[0] for item in content]
         if args['keywords']:
             # searching using a phrase
             phrase = keyword = ' '.join(args['keywords'])
-            keywords = [i for i in keywords if i.startswith(phrase)]
+            keywords = database.find(lambda r: r.title().startswith(phrase))
+        else
+            keywords = database.get_remindmes()
         keywords.sort()
         num = len(keywords)
-        print_out(_success if num > 0 else _error,
-              'Found {0} remindme keywords:'.format(num)
-        )
+        console.success('Found {0} remindme keywords:'.format(num))
         number = 0
         remindmes = ""
         for i in keywords:
           number += 1
           remindmes = ''.join([remindmes, '%-2d- %s\n' % (number, i)])
-        print_out(_default, remindmes)
+        console.log(remindmes)
         return
 
     if args['add']:
-        keyword = ' '.join(args['add'])
-        results = db.search(content, keyword)
+        title = ' '.join(args['add'])
+        results = database.find_by_title(title)
         if results:
-            print_out(_error, "A Remindme already has that name")
+            console.error("A Remindme already has that title")
             return
-        user_input = []
-        print('Enter what you remember now:\n{0}'.format(_default))
-        while 1:
-            try:
-                words = get_input()
-                if words == ':end':
-                    break
-                user_input.append(words)
-            except KeyboardInterrupt:
-                break
-        new_content = '\n'.join(user_input)
+        message = "Enter what you remember now"
+        content = console.get_long_input(message)
 
-        if new_content is "":
-            print_out(_error, "What are we to save?")
+        if user_input is "":
+            console.error("We have nothing to save!")
             return
 
-        if db.add(content, keyword, new_content):
-            print_out(_success, 'RemindMe will remind you next time')
+        if database.new_remindme(title, content):
+            console.success('RemindMe will remind you next time.')
         else:
-            print_out(_error, 'RemindMe failed to get that in memory.')
+            console.error('RemindMe failed to get that in memory.')
 
     if args['in']:
-        keyword = ' '.join(args['in'])
-        new_content = sys.stdin.read().strip()
-        if new_content == '':
-            print_out(_error, 'RemindMe got no data')
+        title = ' '.join(args['in'])
+        content = sys.stdin.read().strip()
+        if content is '':
+            console.error('RemindMe got no data!')
         else:
-            if db.add(content, keyword, new_content):
-                print_out(_success, 'RemindMe will remind you next time')
+            if database.new_remindme(title, content):
+                console.success('RemindMe will remind you next time')
             else:
-                print_out(_error, 'RemindMe failed to get that in memory.\n\
+                console.error('RemindMe failed to get that in memory.\n\
 Maybe there is already another RemindMe with the same keyword.')
 
     if args['remove']:
-        keyword = ' '.join(args['remove'])
-        if db.remove(content, keyword):
-            print_out(_success, 'RemindMe content successfully removed')
+        title = ' '.join(args['remove'])
+        if database.remove(lambda r: r.title() == title):
+            console.success('remindme successfully removed')
         else:
-            print_out(_error, 'RemindMe can NOT remove that. Check if \
-the keywords really exist with me.')
+            console.error('can NOT remove that. Check if the remindme \
+really exists with me.')
 
     if args['remove_all']:
-        confirm = get_input("Remove All Remindmes(yes/NO)")
+        confirm = console.get_input("remove All Remindmes(yes/NO)")
         if confirm.strip().lower() != "yes":
-             return print_out(_error, "Removal cancelled")
-        keywords = [i[0] for i in content]
-        for keyword in keywords:
-            if db.remove(content, keyword):
-                print_out(_success,
-                          'Remindme removed: {0}'.format(keyword))
-            else:
-                print_out(_error,
-                          'Remindme failed to remove: {0}'.format(keyword))
+             return console.error("removal cancelled")
+        if database.remove_all():
+            console.success('removed all of them')
+        else:
+            console.error('failed to remove all')
 
     if args['keywords']:
-        keyword = ' '.join(args['keywords'])
-        results = db.search(content, keyword)
-        if results:
-            print_out(_success, 'RemindMe Reminding you:')
-            results = results.split("\n")
+        title = ' '.join(args['keywords'])
+        remindme = database.find_by_title(title)
+        if remindme:
+            console.success('Reminding you:')
+            lines = remindme.content().split("\n")
             number = 0
-            for line in results:
+            for line in lines:
                 number += 1
-                print_out(_priority, "%-2d " % (number), newline=False)
-                print_out(_default, line)
+                console.info("%-2d " % (number), newline=False)
+                console.log(line)
         else:
-            print_out(_error, 'RemindMe: I too can\'t remember that')
+            console.error('I too can\'t remember that')
 
 
 if __name__ == '__main__':
