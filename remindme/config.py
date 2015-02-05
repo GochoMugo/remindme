9986f284db551fe87e5462dcfd622016884aef7e
Gocho Mugo <mugo@forfuture.co.ke>
2015-02-05 18:26:23 +0300

    Configuration file
    All configuration values will be placed in "remindme/config.py". This
    allows a single point of reference.

---
+++ remindme/config.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import colorama
+
+
+# project metadata
+__version__ = "0.2.1"
+LICENSE = "https://github.com/GochoMugo/remindme/blob/master/LICENSE"
+
+
+# paths
+PATHS = {}
+PATHS["home"] = os.path.expanduser("~")
+PATHS["db_file"] = os.path.join(PATHS["home"], ".remindme.db")
+
+
+# colors
+COLORS = {}
+COLORS["default"] = colorama.Fore.WHITE
+COLORS["error"] = colorama.Fore.RED
+COLORS["priority"] = colorama.Fore.MAGENTA
+COLORS["reset"] = colorama.Style.RESET_ALL
+COLORS["success"] = colorama.Fore.GREEN
+
+
+# python version
+PY2 = sys.version_info[0] == 2
+PY3 = sys.version_info[0] == 3
