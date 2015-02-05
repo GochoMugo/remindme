7482fdee008648f2c4c4de2f1c17ea17551b5fb0
Gocho Mugo <mugo@forfuture.co.ke>
2015-02-05 20:37:35 +0300

    Properties added to remindmes
    Remindmes should have properties. Deleting a remindme will
    mark a property "deleted" to true.
    
    Some of the functions on the remindme database will have to be
    decorated to remove the deleted remindmes from queries.

--- remindme/models.py
+++ remindme/models.py
@@ -17,7 +17,8 @@ class Remindme:
         self.__title = title
         self.__content = content
         self.__db = db
-        self.__cursor = db.cursor()
+        self.__props = {}
+        self.__props["deleted"] = False
 
     def title(self, title=None):
         '''Set or return title of the remindme.'''
@@ -33,12 +34,16 @@ class Remindme:
         self.__content = content
         return self
 
-    def db(self, db=None):
+    def database(self, db=None):
         '''Set or return database reference for the remindme.'''
         if db is None:
             return self.__db
         self.__db = db
         return self
+
+    def props(self):
+        '''Return properties of the remindme.'''
+        return self.__props
 
     def save(self):
         '''Save remindme into database.'''
@@ -60,6 +65,7 @@ class Remindme:
             sql = sql.format(self.__title)
             self.__cursor.execute(sql)
             self.__db.commit()
+            self.__props["deleted"] = True
             return self
         except:
             self.__db.rollback()
@@ -89,8 +95,12 @@ class RemindmeDatabase:
     def new_remindme(self, title, content):
         '''Create a new remindme in the database.'''
         remindme = Remindme(title, content, self.__db)
-        self.__remindmes.append(remindme)
-        return remindme
+        try:
+            remindme.save()
+            self.__remindmes.append(remindme)
+            return remindme
+        except:
+            return None
 
     def load_remindmes(self):
         '''Load remindmes from the database.'''
@@ -103,19 +113,33 @@ class RemindmeDatabase:
             pass
         return self
 
+    def __filter_out(self, func):
+        self.__remindmes = [r for r in self.__remindmes
+            if r.props()["deleted"] is False]
+        return func()
+
+    @self.__filter_out
     def get_remindmes(self):
         '''Return remindmes from database.'''
         return self.__remindmes
 
+    @self.__filter_out
     def save_remindmes(self):
         '''Save all remindmes.'''
         for remindme in self.__remindmes:
             remindme.save()
 
-    def find(self, title):
-        '''Find the remindme.'''
-        for remindme in self.__remindmes:
-            if remindme.title() is title:
-                return remindme
-        return None
+    @self.__filter_out
+    def find(self, qualify):
+        '''Search through the remindmes.'''
+        return [x for x in self.__remindmes if qualify(x)]
 
+    def find_by_title(self, title):
+        '''Find the remindme by title'''
+        return self.find(lambda remindme: remindme.title() == title)
+
+    def remove(self, qualify):
+        '''Remove a remindme from database.'''
+        remindme = self.find(qualify)
+        remindme.delete()
+        return remindme
