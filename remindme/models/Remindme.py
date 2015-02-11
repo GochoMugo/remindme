f221bed2e8892a98412df4e9e7a334ae858c37ce
Gocho Mugo <mugo@forfuture.co.ke>
2015-02-12 01:50:17 +0300

    Neat Models: Remindme and RemindmeRepository
    Remindme serves as an entity representing a single remindme by user.
    RemindmeRepository serves as a repository for storing and retreiving
    remindmes.
    All models have been placed in remindme/models and in separate files
    for better organisation.
    
    Note:
      * API change causes tests to fail

---
+++ remindme/models/Remindme.py
@@ -0,0 +1,65 @@
+'''
+A Remindme is an Entity representing a single user-defined remindme.
+'''
+
+
+class Remindme:
+    '''A user's single remindme.'''
+
+    def __init__(self, title, content, repository=None):
+        '''Creates new remindme.'''
+        self.__title = title
+        self.__content = content
+        self.__repository = repository
+        self.__props = {}
+        self.__props["saved"] = False
+        self.__props["deleted"] = False
+
+    def set_title(self, title):
+        '''Set title of this remindme.'''
+        self.__title = title
+        return self
+
+    def get_title(self):
+        '''Return title of this remindme.'''
+        return self.__title
+
+    def set_content(self, content):
+        '''Set content of this remindme.'''
+        self.__content = content
+        return self
+
+    def get_content(self):
+        '''Return content of this remindme.'''
+        return self.__content
+
+    def set_repository(self, repository):
+        '''Set repository reference for this remindme.'''
+        self.__repository = repository
+        return self
+
+    def get_repository(self):
+        '''Return repository reference for this remindme.'''
+        return self.__repository
+
+    def get_props(self):
+        '''Return properties of this remindme.'''
+        return self.__props
+
+    def save(self):
+        '''Save remindme into repository.'''
+        if self.__repository is not None:
+            status = self.__repository.insert_remindme(self)
+            self.__props["saved"] = status
+            if status is False:
+                raise Exception()
+        return self
+
+    def delete(self):
+        '''Deletes the remindme from repository.'''
+        if self.__repository is not None:
+            status = self.__repository.remove_remindme(self)
+            self.__props["deleted"] = status
+            if status is False:
+                raise Exception()
+        return self
---
+++ remindme/models/RemindmeRepository.py
@@ -0,0 +1,99 @@
+'''
+A RemindmeRepository is a Repository for storing and retrieving Remindmes.
+'''
+
+import sqlite3
+
+
+class RemindmeRepository:
+    '''Repository of Remindmes.'''
+
+    def __init__(self, db_file):
+        '''Create a sqlite3 database for remindmes.'''
+        self.__db = None
+        self.__cursor = None
+        self.__remindmes = []
+        with sqlite3.connect(db_file) as db:
+            self.__db = db
+            self.__cursor = db.cursor()
+            try:
+                sql = 'CREATE TABLE IF NOT EXISTS remindmes(title, content)'
+                self.__cursor.execute(sql)
+                # sql = 'CREATE UNIQUE INDEX indices ON remindmes(title)'
+                # self.__cursor.execute(sql)
+                self.__db.commit()
+                self.restore_remindmes()
+            except Exception:
+                raise Exception()
+
+    def __register_remindme(self, remindme):
+        self.__remindmes.append(remindme)
+
+    def restore_remindmes(self, title, content):
+        '''Restores previously stored remindmes from the database.'''
+        try:
+            sql = 'SELECT title, content FROM remindmes'
+            for item in self.__cursor.execute(sql).fetchall():
+                remindme = Remindme(item[0], item[1], self)
+                self.__register_remindme(remindme)
+        except sqlite3.OperationalError:
+            pass
+        return self
+
+    def insert_remindme(self, remindme):
+        '''Insert remindme into this repository.'''
+        try:
+            sql = 'INSERT INTO remindmes VALUES (?,?)'
+            self.__cursor.execute(sql, (remindme.get_title(), remindme.get_content(),))
+            self.__db.commit()
+            self.__register_remindme(remindme)
+            return True
+        except sqlite3.OperationalError:
+            self.__db.rollback()
+            return False
+
+    def create_remindme(self, title, content):
+        '''Creates a new remindme in this repository.'''
+        remindme = Remindme(title, content, self)
+        try:
+            self.insert_remindme(remindme)
+            return remindme
+        except:
+            return None
+
+    def remove_remindme(self, remindme):
+        '''Remove remindme from this repository.'''
+        try:
+            sql = 'DELETE FROM remindmes WHERE keyword == "{0}" '
+            sql = sql.format(remindme.get_title())
+            self.__cursor.execute(sql)
+            self.__db.commit()
+            return True
+        except:
+            self.__db.rollback()
+            return False
+
+    def __filter_out_deleted(self):
+        '''Filters out deleted remindmes.'''
+        self.__remindmes = [r for r in self.__remindmes
+            if r.props()["deleted"] is False]
+
+    def get_remindmes(self):
+        '''Return remindmes from database.'''
+        self.__filter_out_deleted()
+        return self.__remindmes
+
+    def save_remindmes(self):
+        '''Save all remindmes.'''
+        self.__filter_out_deleted()
+        for remindme in self.__remindmes:
+            remindme.save()
+
+    def find(self, qualify):
+        '''Search through the remindmes.'''
+        self.__filter_out_deleted()
+        return [x for x in self.__remindmes if qualify(x)]
+
+    def find_by_title(self, title):
+        '''Find the remindme by title.'''
+        return self.find(lambda remindme: remindme.get_title() == title)
