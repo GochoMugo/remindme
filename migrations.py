10fc17b8c99ed1f5003065a3d7a97403bec472ab
GochoMugo <mugo@forfuture.co.ke>
2015-10-01 22:53:24 +0300

    update migrations

--- migrations.py
+++ migrations.py
@@ -9,7 +9,8 @@ import remindme
 
 
 CURRENT_VERSION = None
-NEW_VERSION = None
+NEW_VERSION = remindme.__version__
+MIGRATIONS = {}
 
 console = remindme.utils.Console("migration")
 db_file = remindme.config.PATHS["db_file"]
@@ -18,32 +19,38 @@ db_file = remindme.config.PATHS["db_file"]
 def detect_versions():
     '''Detects the current version installed.'''
     global CURRENT_VERSION
-    global NEW_VERSION
     cwd = os.getcwd()
     sys.path.remove(cwd)
-    old_mod = sys.modules["remindme"]
-    del sys.modules["remindme"]
     try:
-        import remindme
-        CURRENT_VERSION = remindme.__version__
-        console.info("currently installed version: {0}".format(CURRENT_VERSION))
+        # user may not have installed remindme EVER
+        unload_remindme()
+        import remindme as r
+        CURRENT_VERSION = r.__version__ or "unversioned"
     except:
         console.error("failed to detect currently installed version")
         pass
+    # clean-up
     sys.path.append(cwd)
-    sys.modules["remindme"] = old_mod
-    import remindme # we have to re-import here!!!
-    NEW_VERSION = remindme.__version__
+    unload_remindme()
+    import remindme
+    console.info("currently installed version: {0}".format(CURRENT_VERSION))
     console.info("new version to install: {0}".format(NEW_VERSION))
+
+
+def unload_remindme():
+    targets = [x for x in sys.modules if x.startswith("remindme")]
+    for mod in targets:
+        del sys.modules[mod]
 
 
 def migrate():
     '''Handles invoking migrations.'''
     console.info("process migrations")
-    global CURRENT_VERSION 
+    global CURRENT_VERSION
     OLD_VERSION = CURRENT_VERSION
-    if CURRENT_VERSION == "0.2.1" or CURRENT_VERSION is None:
-        CURRENT_VERSION = migrate_1() # 1st ever migration
+    func = MIGRATIONS.get(CURRENT_VERSION, None)
+    if func is None:
+        CURRENT_VERSION = func()
     if CURRENT_VERSION is None:
         sys.exit(1)
     if OLD_VERSION != CURRENT_VERSION: # there was change/migration
@@ -52,13 +59,13 @@ def migrate():
 
 
 def migrate_1():
-    '''Migrates from 0.2.1 to 0.3.0
+    '''Migrates to 0.3.0
 
     Renames database column, `keywords`, to `title`.
     See "http://stackoverflow.com/questions/805363/how-do-i-rename-a-column-in
     -a-sqlite-database-table" for more information.
     '''
-    console.info("migrating from 0.2.1 to 0.3.0")
+    console.info("migrating to 0.3.0")
     sql_rename_orig_tbl = "ALTER TABLE remindmes RENAME TO tmp_remindmes;"
     sql_create_tbl = "CREATE TABLE remindmes(title, content);"
     sql_copy_old_tbl = '''
@@ -74,7 +81,7 @@ def migrate_1():
         cursor.execute(sql_copy_old_tbl)
         cursor.execute(sql_drop_orig_tbl)
         db.commit()
-        console.success("success migrating from 0.2.1 to 0.3.0")
+        console.success("success migrating to 0.3.0")
         return "0.3.0"
     except Exception as err:
         db.rollback()
@@ -83,10 +90,42 @@ def migrate_1():
         return None
 
 
+def migrate_2():
+    '''Migrate to 0.5.0'''
+    console.info("migrating to 0.5.0")
+    sql_rename_orig_tbl = "ALTER TABLE remindme RENAME TO tmp_remindmes;"
+    sql_create_tbl = "CREATE TABLE remindmes(title, content BLOB, salt BLOB);"
+    sql_copy_old_tbl = "INSERT INTO remindmes(title, content) SELECT * FROM tmp_remindmes;"
+    sql_drop_orig_tbl = "DROP TABLE tmp_remindmes;"
+    sql_create_sys_tbl = "CREATE TABLE sys(key, value);"
+    try:
+        db = sqlite3.connect(db_file)
+        cursor = db.cursor()
+        cursor.execute(sql_rename_orig_tbl)
+        cursor.execute(sql_create_tbl)
+        cursor.execute(sql_copy_old_tbl)
+        cursor.execute(sql_drop_orig_tbl)
+        cursor.execute(sql_create_sys_tbl)
+        db.commit()
+        console.success("success migrating to 0.5.0")
+        return "0.5.0"
+    except Exception as err:
+        db.rollback()
+        console.error("error migrating")
+        console.error(err)
+        return None
+
+
+MIGRATIONS["unversioned"] = migrate_1
+MIGRATIONS["0.2.1"] = migrate_1
+MIGRATIONS["0.3.0"] = migrate_2
+MIGRATIONS["0.4.0"] = migrate_2
+
+
 def start():
     '''Start migrations.'''
     detect_versions()
-    migrate()
+    # migrate()
 
 
 if __name__ == "__main__":
