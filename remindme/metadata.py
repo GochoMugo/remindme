f80c3b47fe2eab0c19d423547a5bef152bab5431
GochoMugo <mugo@forfuture.co.ke>
2016-03-20 11:35:38 +0300

    v0.6.1
    See ChangeLog

--- remindme/metadata.py
+++ remindme/metadata.py
@@ -12,5 +12,5 @@ Contribute to the Project at https://github.com/GochoMugo/remindme
 
 
 # project metadata
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 LICENSE = "https://github.com/GochoMugo/remindme/blob/master/LICENSE"
